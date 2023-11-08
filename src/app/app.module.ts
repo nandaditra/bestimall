ff89841dcb02f631f1e8e53fc131e3ac50f3c095
nandaditra <nandadditra56@gmail.com>
2023-11-08 12:30:06 +0700

    delete category folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { HomeModule } from './modules/home/home.module';
 import { ContentlayoutComponent } from './layout/contentlayout/contentlayout.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DetailProductModule } from './modules/detail-film/detail-product.module';
-import { CategoryModule } from './modules/category/category.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { ElectronicsModule } from './modules/electronics/electronics.module';
+import { JeweleryModule } from './modules/jewelery/jewelery.module';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,8 @@ import { AuthModule } from './modules/auth/auth.module';
     HomeModule,
     DetailProductModule,
     SharedModule,
-    CategoryModule,
+    ElectronicsModule,
+    JeweleryModule,
     AuthModule,
   ],
   providers: [],
