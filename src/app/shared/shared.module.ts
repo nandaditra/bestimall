24e671320b93b546fc33700b5c70da790952ce1c
nandaditra <nandadditra56@gmail.com>
2023-11-11 22:25:50 +0700

    add material module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductItemComponent } from './component/product-item/product-item.component';
 import { RouterLink } from '@angular/router';
+import { ControlMessaggeComponent } from './component/control-messagge/control-messagge.component';
+import { MaterialModule } from './material.module';
+import { SpinnerComponent } from './component/spinner/spinner.component';
 
 @NgModule({
-  declarations: [ProductItemComponent],
+  declarations: [ProductItemComponent, ControlMessaggeComponent, SpinnerComponent],
   imports: [
     CommonModule,
     RouterLink
   ],
   exports: [
-    ProductItemComponent
+    ProductItemComponent,
+    SpinnerComponent,
+    MaterialModule
   ]
 })
 export class SharedModule { }
