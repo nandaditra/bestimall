33f8cbee16937c577a4e7822aa300d491baa20f7
nandaditra <nandadditra56@gmail.com>
2023-11-13 16:02:48 +0700

    add header authentication

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth/auth.service';
+import { UserService } from 'src/app/core/service/user/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent{
     logo= "../../../assets/images/logo/bestimall.png"
+    authenticated: boolean = false
+
+    constructor(
+        private authService:AuthService,
+        private userService:UserService,
+        private router:Router
+      ){
+    }
+
+    ngOnInit():void{
+        if(this.userService.getUser()) {
+           this.authenticated = true
+        }
+    }
+
+    async logOut(){
+       this.userService.outUser()
+       this.authenticated = false;
+       return this.authService.signOut()
+       .then(() =>
+         this.router.navigateByUrl('login')
+       )
+    }
 }
